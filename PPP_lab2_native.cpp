783650c23cc7082b156a4657525b40c0679efc20
leriashes <valeriya-shevchenko-2002@mail.ru>
2024-10-12 01:32:07 +0700

    Сохранение изображения

--- PPP_lab2_native.cpp
+++ PPP_lab2_native.cpp
@@ -1,10 +1,14 @@
-﻿#define STB_IMAGE_IMPLEMENTATION  // Определяем макрос для реализации функций
+﻿#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#define _CRT_SECURE_NO_WARNINGS
 #include "stb_image.h"
+#include "stb_image_write.h"
 #include <iostream>
 #include <windows.h>
 #include <string>
 #include <chrono>
 #include <thread>
+#include <vector>
 using namespace std;
 
 int main()
@@ -25,10 +29,20 @@ int main()
             << ", Channels: " << channels << endl;
     }
     else {
-        cerr << "Не удалось загрузить изображение!" << std::endl;
+        cerr << "Не удалось загрузить изображение!" << endl;
     }
 
+    vector<unsigned char> img(data, data + (width * height * channels));
     stbi_image_free(data);
+
+    vector<unsigned char> result(width * height * channels);
+
+    if (stbi_write_png("result.jpg", width, height, channels, result.data(), width * channels)) {
+        cout << "Изображение сохранено как result.jpg" << endl;
+    }
+    else {
+        cerr << "Ошибка при сохранении изображения!" << endl;
+    }
 
     return 0;
 }
